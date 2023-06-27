c03bd4057c1d94b4ab086ed833d4a8f092186279
Madhu Balaji <maddyonline@gmail.com>
2023-06-27 14:52:20 -0400

    Develop (#5)
    * Implement CDK NAG and other updates
    
    * update readme
    
    * Update SecGroup and readme
    
    * Update README.md
    
    * format readme
    
    * update image
    
    * update image
    
    * update images
    
    ---------
    
    Co-authored-by: madhu balaji <madhusba@amazon.com>

---
+++ constructs/test/dih_cdk.test.ts
@@ -0,0 +1,21 @@
+import * as cdk from 'aws-cdk-lib';
+import { DIHCdkStack } from '../lib/dih-cdk-stack';
+import { AwsSolutionsChecks } from 'cdk-nag'
+import { App, Aspects } from 'aws-cdk-lib';
+import { DIHStackProps } from '../lib/dih-stack-props';
+
+const app = new App();
+const props: DIHStackProps = {
+  instanceType: "t2.micro",
+  vpcCidr: "10.0.0.0/24",
+};
+//Test case for DIH CDK
+describe('DIH CDK Test', () => {
+  test('DIH CDK Stack', () => {
+    const stack = new DIHCdkStack(app, 'DIH-CDK-Stack', props);
+  
+  });
+});
+// Nag checks
+
+Aspects.of(app).add(new AwsSolutionsChecks({verbose:true}));
